88665e8802f12e45f31d26e36ab4693b0c61d972
--get <miron@machnicki.com.pl>
2018-01-28 13:29:00 +0000

    Webpack plus server

---
+++ client/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist', {
+  'index': ['../src/index.html']
+}));
+app.listen(8080, () => console.log('App listening on port 8080!'));
